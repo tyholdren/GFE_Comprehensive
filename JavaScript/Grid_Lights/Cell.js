01fc4968982b0b77680bb1a5412f1343e58bf9d1
tyholdren <tsholdre@syr.edu>
2024-06-04 16:56:50 +0200

    set active cells logic

--- JavaScript/Grid_Lights/Cell.js
+++ JavaScript/Grid_Lights/Cell.js
@@ -1,12 +1,15 @@
 export class Cell {
-  constructor(data) {
+  constructor(data, index) {
     this.data = data;
+    this.index = index;
+    this.isDisabled = false;
   }
 
   render() {
     const $button = document.createElement('button');
     $button.classList.add('cell');
-    $button.textContent = 'button';
+    $button.textContent = 'cell';
+    $button.id = `cell-${this.index}`;
     return $button;
   }
 }
--- JavaScript/Grid_Lights/app.js
+++ JavaScript/Grid_Lights/app.js
@@ -7,19 +7,36 @@ export class App {
       [1, 0, 1],
       [1, 1, 1],
     ];
+    this.activatedCells = [];
     this.isDeactivating = false;
   }
 
   initialize() {
     const fragment = document.createDocumentFragment();
-    const grid = this.config.flat().map(data => {
-      const curCell = new Cell(data);
-      const $cellEl = curCell.render();
-      return $cellEl;
+    const grid = this.config.flat().map((data, index) => {
+      if (data) {
+        const curCell = new Cell(data, index);
+        const $cellEl = curCell.render();
+        return $cellEl;
+      } else {
+        return document.createElement('span');
+      }
     });
     fragment.append(...grid);
     this.$appContainer.append(fragment);
+    this.$appContainer.addEventListener('click', event => {
+      if (event.target.textContent === 'cell') {
+        const activeCell = event.target;
+        this.activateCell(activeCell);
+      }
+    });
     return this.$appContainer;
+  }
+
+  activateCell(cell) {
+    this.activatedCells.push(cell);
+    cell.isDisabled = true;
+    cell.classList.add('active-cell');
   }
 }
 
