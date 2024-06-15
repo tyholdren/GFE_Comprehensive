6710a0370c205d2f507f18c4c2848c9525154a53
tyholdren <tsholdre@syr.edu>
2024-06-15 11:49:33 +0200

    promise.reject

---
+++ Coding_Challenges/promiseReject.ts
@@ -0,0 +1,10 @@
+/**
+ * https://www.greatfrontend.com/questions/javascript/promise-reject
+ * @param {*} reason
+ * @returns Promise
+ */
+export default function promiseReject(reason) {
+  return new Promise((_, reject) => {
+    reject(reason);
+  });
+}
