290f3f8ceb4b8c3b94460a8d73b76b515a83cff7
tyholdren <tsholdre@syr.edu>
2024-06-21 10:37:41 -0400

    type utilities problem

---
+++ Projects/billing-history-section/js/typeUtilities.js
@@ -0,0 +1,25 @@
+// https://www.greatfrontend.com/questions/javascript/type-utilities
+
+export function isBoolean(value) {
+  return typeof value === 'boolean';
+}
+
+export function isNumber(value) {
+  return typeof value === 'number';
+}
+
+export function isNull(value) {
+  return value === null;
+}
+
+export function isString(value) {
+  return typeof value === 'string';
+}
+
+export function isSymbol(value) {
+  return typeof value === 'symbol';
+}
+
+export function isUndefined(value) {
+  return value === undefined;
+}
